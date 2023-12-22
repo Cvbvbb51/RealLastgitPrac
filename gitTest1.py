29494654010f41736418feb322ff8d8a41442d79
SongJH <--replace-all>
2023-12-22 17:12:42 +0900

    freshman first commit

--- gitTest1.py
+++ gitTest1.py
@@ -32,3 +32,35 @@ class MNISTClassificationModel_FC_only(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.layers(x)
         return x
+
+
+# freshman 작업
+class MNISTClassificationModel_CNN(nn.Module):
+    def __init__(self):
+        super(MNISTClassificationModel_CNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            1, 32, kernel_size=3, stride=1, padding=1
+        )  # 1=채널수, 32=출력채널(필터의 수), 필터크기 -> (32, H, W)
+        # self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1) # output: (64, H, W)
+
+        self.pool = nn.MaxPool2d(
+            kernel_size=2, stride=2, padding=0
+        )  # output: (H/2, W/2)
+
+        self.fc1 = nn.Linear(32 * 14 * 14, 64)
+        # self.fc1 = nn.Linear(64 * 7 * 7, 64)
+
+        self.fc2 = nn.Linear(64, 10)  # 출력 계층으로 바로 연결
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))  # (32, 14, 14)
+        # x = self.pool(torch.relu(self.conv2(x))) # (64, 7, 7)
+
+        x = x.view(-1, 32 * 14 * 14)  # Flatten
+        # x = x.view(-1, 64 * 7 * 7) # Flatten
+
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
